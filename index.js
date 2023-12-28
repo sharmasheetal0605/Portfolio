9b32ff8c5bc6f15acdc70983cd9849e0b8e3add9
sharmasheetal0605 <136837162+sharmasheetal0605@users.noreply.github.com>
2023-12-28 19:15:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//document.querySelector('.cross').style.display='none';
+//document.querySelector('.sidebargo').style.display='none';
+//document.querySelector('#bars').style.display='#bars';
+//document.querySelector('#bars').addEventListener("click", ()=>{
+    //document.querySelector('.main-content').classList.toggle('sidebargo');
+//})
+document.querySelector('.btn').addEventListener("click",()=>{
+    confirm("Do you want to download CV");
+});
+document.querySelector('.formbtn').addEventListener("click",()=>{
+    confirm("Done");
+});
+document.querySelector('.suggestbtn').addEventListener("click",()=>{
+    confirm("We got your suggestion.");
+});
+
